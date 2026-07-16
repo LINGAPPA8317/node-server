d69a3aa103f49840078b53792ce04cd71b8175aa
Lingappa M <lingumd@LAPTOP-82NUEGEF.localdomain>
2026-07-16 17:14:10 +0000

    updated files

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from Docker Compose!");
+
+    if (req.url === "/") {
+        fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+
+    } else if (req.url === "/style.css") {
+        fs.readFile(path.join(__dirname, "public", "style.css"), (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/css" });
+            res.end(data);
+        });
+
+    } else if (req.url === "/script.js") {
+        fs.readFile(path.join(__dirname, "public", "script.js"), (err, data) => {
+            res.writeHead(200, { "Content-Type": "application/javascript" });
+            res.end(data);
+        });
+
+    } else {
+        res.writeHead(404);
+        res.end("Page Not Found");
+    }
 });
 
 server.listen(PORT, () => {
